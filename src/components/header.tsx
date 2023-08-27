33b608b580d526825a944046e7a1963eddbbd18b
chris434 <christopherpearce589@gmail.com>
2023-08-27 09:42:06 +0100

    home link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,11 @@ type ChildrenProps = {
 
 export function Header({children}:ChildrenProps) {
     return <div className="sticky top-0">
-        <header className="bg-blue-400 p-5 sticky"><h1 className="text-2xl text-white">world bankHolidays</h1></header>
+        <header className="bg-blue-400 p-5 sticky">
+            <a href="/">
+                <h1 className="text-2xl text-white">world bankHolidays</h1>
+            </a>
+        </header>
         {children}
     </div>
 }
