6c717ffa3e201be505efdefed9a8e83faa2add21
chris434 <christopherpearce589@gmail.com>
2023-08-27 10:49:21 +0100

    toggle menu

--- src/views/bankHolidays.tsx
+++ src/views/bankHolidays.tsx
@@ -1,33 +1,47 @@
 import { useRouter } from 'next/router'
-import {useRef,Fragment } from 'react'
+import Image from 'next/image'
+import {useRef,Fragment, useState } from 'react'
 import { bankHolidaysProps } from '@/types/bankHolidaysType'
 import { Flag } from '@/components/flag'
 import { Button } from '@/components/button'
 import { BankHoliday } from '@/components/bankHoliday'
 import { Header } from '@/components/header'
+import chevronDown from '@/icons/chevron-down-solid.svg'
+import chevronUp from '@/icons/chevron-up-solid.svg'
 
 export function BankHolidaysView({ bankHolidays,year,countryCode,country,nextBankHoliday }: bankHolidaysProps) {
     const router = useRouter()
     const currentYear=new Date().getFullYear()
     const yearInput = useRef<HTMLInputElement>(null)
+    const [menuToggle,toggleMenu]=useState(true)
     
     return <main>
         <Header>
-        <section className='flex sm:flex-row flex-col justify-between items-center p-5 bg-white '>
+            <div className="bg-white">
+                <button className='cursor-pointer m-3 mt-0 ml-auto block' onClick={()=>toggleMenu(menuToggle=> !menuToggle)}>
+                     <Image className=' sm:hidden block' src={menuToggle?chevronUp: chevronDown} alt={menuToggle?'open':'closed'} width="30" height="30" />
+                </button>
+              
+                {menuToggle&&
+                <section className='flex sm:flex-row flex-col justify-between items-center p-5 sm:pt-5 pt-0 '>
+    
             <div className='flex items-center gap-5 sm:mb-0 mb-3'>
             <Flag countryName={country.countryName } countryCode={country.countryCode} />
             <h2 className='text-2xl'>{country.countryName}</h2>
-            </div>
+                </div>
                 <div className="flex p-3 bg-blue-400 justify-around rounded-md gap-5">
              <form className="w-[5rem]">
                   <input ref={yearInput} min="1973" max="2073"  className='bg-transparent border-white border-2 text-white p-2 w-full ' type='number' defaultValue={year || currentYear} />        
             </form>
           
-            <Button  onClick={()=>yearInput.current?.value&& router.push(`/${countryCode}/${yearInput.current.value}`)}>look up year</Button>
-            </div>
-        </section>
+                    <Button onClick={() => yearInput.current?.value && router.push(`/${countryCode}/${yearInput.current.value}`)}>look up year</Button>
+                </div>
+            
+                    
+                </section>
+                   }
+          </div>
         </Header>
-       
         <hr className='border-1 border-gray-500 ' />
         <ul className='flex justify-center list-none gap-5 sm:flex-row  flex-col-reverse w-full mt-5'>
         
